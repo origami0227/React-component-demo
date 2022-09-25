552f97010030d59814f6e4bfa3b32d29c33eafed
origami <1071242996@qq.com>
2022-09-25 13:30:37 +0800

    两种组件写法

---
+++ src/index.js
@@ -0,0 +1,46 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      爸爸
+      <Son />
+    </div>
+  );
+}
+
+class Son extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      n: 0
+    };
+  }
+  add() {
+    this.setState({ n: this.state.n + 1 });
+  }
+  render() {
+    return (
+      <div className="Son">
+        儿子 n:{this.state.n}
+        <button onClick={() => this.add()}>+1</button>
+        <Grandson />
+      </div>
+    );
+  }
+}
+const Grandson = () => {
+  const [n, setN] = React.useState(0);
+  return (
+    <div className="Grandson">
+      孙子 n:{n}
+      <button onClick={() => setN(n + 1)}>+1</button>
+    </div>
+  );
+};
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
